38d10bccb2dfd66d4f45150f09a0ef2b039fcec5
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2023-04-05 00:36:41 +0100

    Add corrections to files

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,7 +3,7 @@
 exec {'update':
   provider => shell,
   command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+  before   => Exec['install nginx'],
 }
 
 exec {'install nginx':
@@ -16,10 +16,10 @@ exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
   command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
+  before      => Exec['restart nginx'],
 }
 
-exec { 'restart Nginx':
+exec { 'restart nginx':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
